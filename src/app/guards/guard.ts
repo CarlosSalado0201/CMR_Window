e219685ea8093d9e6c6f73b7f7359d7147e50e99
Kitzy9 <sack99013@gs.utm.mx>
2025-11-19 13:31:32 -0600

    Rebuild

--- src/app/guards/guard.ts
+++ src/app/guards/guard.ts
@@ -13,7 +13,7 @@ export class AuthGuard implements CanActivate {
   if (this.auth.usuarioActual) {
     return true;
   } else {
-    this.router.navigate(['/login']);
+    this.router.navigate(['login']);
     return false;
   }
 }
