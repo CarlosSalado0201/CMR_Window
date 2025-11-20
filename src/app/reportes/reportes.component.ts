c2f994fd7e4a6ae9082a31196f2f910873df36cc
Kitzy9 <sack99013@gs.utm.mx>
2025-11-19 19:07:53 -0600

    Rebuild

--- src/app/reportes/reportes.component.ts
+++ src/app/reportes/reportes.component.ts
@@ -364,5 +364,10 @@ export class ReportesComponent implements AfterViewInit, OnInit {
 onImagenesLecturas(event: any) {
   this.imagenesLecturas = Array.from(event.target.files);
 }
+aprobarVerificacion() {
+  this.verificado = true;
+  this.cerrarVerificacion();
+}
+
 
 }
